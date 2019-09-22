e19e29d3942f1c63a79086df419e8c09f6f7ee93
zengjiahua <724630558@qq.com>
2019-09-22 19:37:43 +0800

    first commit

---
+++ Model/Model.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+        public string userImg { get; set; }
+    }
+    public class UserInfoNo : UserInfo
+    {
+        public int num { get; set; }
+    }
+    public class Page
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+    public class Activity
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+        public DateTime endTime { get; set; }
+        public char activityPicture { get; set; }
+        public char activityIntroduction { get; set; }
+        public char summary { get; set; }
+        public char activityVerify { get; set; }
+        public char activityStatus { get; set; }
+        public char userName { get; set; }
+        public char recommend { get; set; }
+        public DateTime recommendTime { get; set; }
+
+    }
+    public class WorkInfo
+    {
+        public int workId { get; set; }
+        public char workName { get; set; }
+        public char workPicture { get; set; }
+        public DateTime uploadTime { get; set; }
+        public char workIntroduction { get; set; }
+        public char workVerify { get; set; }
+        public char userName { get; set; }
+        public int activityId { get; set; }
+        public char recommend { get; set; }
+        public DateTime RecommendTime { get; set; }
+    }
+    public class Comment
+    {
+        public int commentId { get; set; }
+        public int workID { get; set; }
+        public char userName { get; set; }
+        public char commentContent { get; set; }
+        public DateTime commentTime { get; set; }
+    }
+    public class WorkFindPage:Page
+    {
+        public string workName { get; set; }
+    }
+    public class WorkMyPage : Page
+    {
+        public string userName { get; set; }
+    }
+    public class CommentPage : Page
+    {
+        public string workId { get; set; }
+    }
+    public class ActivityNo : Activity
+    {
+        public int num { get; set; }
+    }
+    public class ActivityName
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+    }
+    public class WorkInfoNo:WorkInfo
+    {
+        public int num { get; set; }
+        public string activityName { get; set; }
+    }
+    public class CommentNo:Comment
+    {
+        public int num { get; set; }
+        public string workName { get; set; }
+    }
+}
---
+++ Service/Controllers/UploadController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        //// GET: api/<controller>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpPost]
+        public ActionResult Post(List<IFormFile> files)
+        {
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "img", "Editor");
+            string fName = Guid.NewGuid().ToString().Replace("-", "");
+            var fileName = $"{path}/{fName}";
+            try
+            {
+                var ext = DAL.Upload.Instance.UpImg(files[0], fileName);
+                if (ext == null)
+                    return Json(Result.Err("请上传图片文件"));
+                else
+                {
+                    var file = $"https://{HttpContext.Request.Host.Value}/img/Editor/{fName}{ext}";
+                    return Json(Result.Ok("上传成功", file));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(Result.Err(ex.Message));
+            }
+        }
+    }
+}
